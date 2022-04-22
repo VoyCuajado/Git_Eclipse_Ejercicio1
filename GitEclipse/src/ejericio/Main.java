8813aab9a0377806f727c1388926b04de5904182
Daw2 <rubenlozano.daw@ciudadescolarfp.es>
2022-04-22 12:55:38 +0200

    Inicializo el proyecto

---
+++ GitEclipse/src/ejericio/Main.java
@@ -0,0 +1,11 @@
+package ejericio;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Listado de alumnos 1ro DAW");
+	}
+
+}
