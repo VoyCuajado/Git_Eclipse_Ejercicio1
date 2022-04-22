c48dcc75dcd77a1427bb784ffdc554bace56526c
Daw2 <rubenlozano.daw@ciudadescolarfp.es>
2022-04-22 13:02:26 +0200

    Añadido constructor (3 parametros) en clase Alumno

--- GitEclipse/src/ejericio/Alumno.java
+++ GitEclipse/src/ejericio/Alumno.java
@@ -5,4 +5,10 @@ public class Alumno {
 	private String nombre;
 	private String apellido;
 	private String dni;
+	
+	public Alumno(String nombre, String apellido, String dni) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+	}
 }
